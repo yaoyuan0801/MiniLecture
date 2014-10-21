80e924f5c3d588612796502e5861df24ac6310b0
Yuan Yao <yuan.yao@turn.com>
2014-10-20 17:00:13 -0700

    Add regex expression

--- C++/regex.cpp
+++ C++/regex.cpp
@@ -38,39 +38,94 @@ int countRegularChar(const char* s) {
   }
   return count;
 }
-
-bool isMatch(const char *s, const char *p) {
-  int l1 = strlen(s), l2 = strlen(p);
-  if (countRegularChar(p) > countRegularChar(s)) {
+class WildCard{
+  
+public:
+  bool isMatch(const char *s, const char *p) {
+    int l1 = strlen(s), l2 = strlen(p);
+    if (countRegularChar(p) > countRegularChar(s)) {
+      return false;
+    }
+    vector<bool> res1(l2 + 1), res2(l2 + 1);
+    res1[0] = true;
+    for (int i = 0; i < l2; i++) {
+      if (p[i] == '*') {
+        res1[i + 1] = true;
+      } else {
+        break;
+      }
+    }    
+    for (int i = 0; i < l1; i++) {
+      res2[0] = false;
+      for (int j = 0; j < l2; j++) {
+        if (p[j] != '*') {
+          res2[j + 1] = res1[j] && isCharMatch(s[i], p[j]);
+        } else {
+          res2[j + 1] = res1[j] || res1[j + 1] || res2[j];
+        }
+      }
+      swap(res1, res2);
+    }
+    return res1[l2];
+  }
+};
+class Regex {
+public:
+  bool isCharMatch(char a, char b) {
+    if (b == '.') {
+      return true;
+    }
+    if (a == b) {
+      return true;
+    }
     return false;
   }
-  vector<bool> res1(l2 + 1), res2(l2 + 1);
-  res1[0] = true;
-  for (int i = 0; i < l2; i++) {
-    if (p[i] == '*') {
-      res1[i + 1] = true;
-    } else {
-      break;
-    }
-  }
-  printVector(res1);
   
-  for (int i = 0; i < l1; i++) {
-    res2[0] = false;
-    for (int j = 0; j < l2; j++) {
-      if (p[j] != '*') {
-        res2[j + 1] = res1[j] && isCharMatch(s[i], p[j]);
+  int countRegularChar(const char* s) {
+    int count = 0, i = 0;
+    while (s[i]) {
+      count += (s[i] != '*');
+      i++;
+    }
+    return count;
+  }
+  
+  bool isMatch(const char *s, const char *p) {
+    int l1 = strlen(s), l2 = strlen(p);
+    vector<bool> res1(l2 + 1), res2(l2 + 1);
+    res1[0] = true;
+    for (int i = 1; i < l2; i += 2) {
+      if (p[i] == '*' && p[i - 1] != '*' && res1[i - 1]) {
+        res1[i + 1] = true;
       } else {
-        res2[j + 1] = res1[j] || res1[j + 1] || res2[j];
+        break;
       }
     }
-    swap(res1, res2);
-    printVector(res1);
+    for (int i = 0; i < l1; i++) {
+      res2[0] = false;
+      for (int j = 0; j < l2; j++) {
+        if (p[j] != '*') {
+          res2[j + 1] = res1[j] && isCharMatch(s[i], p[j]);
+        } else {
+          if (j > 0) {
+            res2[j + 1] = res2[j - 1];
+            res2[j + 1] = res2[j + 1] || (res2[j] && isCharMatch(s[i], p[j - 1]));
+            res2[j + 1] = res2[j + 1] || (res1[j + 1] && isCharMatch(s[i], p[j - 1]));
+          }
+        }
+      }
+      swap(res1, res2);
+    }
+    return res1[l2];
   }
-  return res1[l2];
-}
+};
 
 int main(void) {
   char* s = "bbbaab", *p = "a**?***";
-  cout<< isMatch(s, p) <<endl;
+  WildCard wildCard;
+  cout<< wildCard.isMatch(s, p) <<endl;
+  Regex regex;
+  s = "aaba";
+  p = "ab*a*c*a";
+  cout << regex.isMatch(s, p) << endl;
 }
